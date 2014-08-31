e748a70d538bc8bb5e7f9ec8f72e9a09892615bf
nick lehman <nlehman@combitsystems.com>
2014-08-31 00:25:15 -0400

    command line options

--- s.rb
+++ s.rb
@@ -1,7 +1,13 @@
-#! /Users/nlehman/local/bin/ruby
+#! /usr/bin/env ruby
 
 require 'Nokogiri'
 require 'open-uri'
+
+def usage(s)
+	$stderr.puts(s)
+	$stderr.puts("Usage: #{File.basename($0)}: <jsFiddle Id>")
+	exit(2)
+end
 
 def write_jsfiddle_src(src, src_type)
 	File.open('demo.' + src_type, 'w') do |f|
@@ -9,8 +15,15 @@ def write_jsfiddle_src(src, src_type)
 	end
 end
 
-doc = Nokogiri::HTML(open("http://jsfiddle.net/homerj/8wn3w44v/"))
+loop { case ARGV[0]
+	when /^-/ then  usage("Unknown option: #{ARGV[0].inspect}")
+	else break
+end; }
 
-write_jsfiddle_src(doc.css('textarea#id_code_html').text, 'html')
-write_jsfiddle_src(doc.css('textarea#id_code_js').text, 'js')
-write_jsfiddle_src(doc.css('textarea#id_code_css').text, 'css')
+if ARGV.any?
+	doc = Nokogiri::HTML(open("http://jsfiddle.net/#{ARGV[0]}"))
+
+	write_jsfiddle_src(doc.css('textarea#id_code_html').text, 'html')
+	write_jsfiddle_src(doc.css('textarea#id_code_js').text, 'js')
+	write_jsfiddle_src(doc.css('textarea#id_code_css').text, 'css')
+end
